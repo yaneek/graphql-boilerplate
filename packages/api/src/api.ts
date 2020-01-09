ad4e1431eda45be3a01f607c717d33dbb50eb6d9
Grzegorz Marchwinski <yankeepl@gmail.com>
2020-01-09 18:58:08 +0100

    chore(deps): upgrade deps

--- packages/api/src/api.ts
+++ packages/api/src/api.ts
@@ -1,30 +1,34 @@
 import 'reflect-metadata';
 import * as express from 'express';
-import { buildSchema, useContainer } from 'type-graphql';
+import { buildSchema } from 'type-graphql';
 import { ApolloServer } from 'apollo-server-express';
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
 import { registerApiEnums } from './register-enum-types';
 import { APIModule } from './APIModule';
 import { allResolvers } from './resolvers';
+import { INestApplication } from '@nestjs/common';
 
 const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
+  let app: INestApplication;
   console.log('Register graphql enums');
   registerApiEnums();
   console.log('Build graphql schema');
-  const schema = await buildSchema({ resolvers: allResolvers });
+  const schema = await buildSchema({
+    resolvers: allResolvers,
+    container: () => ({
+      get: (someClass: any) => app.get(someClass),
+    }),
+  });
   console.log('Configure Apollo server');
   const graphqlServer = new ApolloServer({ playground: true, schema });
   const server = express();
   graphqlServer.applyMiddleware({ app: server });
   console.log('Configure Nest factory');
-  const app = await NestFactory.create(APIModule, server);
-
-  useContainer({
-    get: (someClass: any) => app.get(someClass),
-  });
+  app = await NestFactory.create(APIModule, new ExpressAdapter(server));
 
   console.log('Starting server');
   app.listen(PORT, () => {
